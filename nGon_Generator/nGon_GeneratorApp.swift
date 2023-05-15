c812ab4988f9fce38d1006d78499252b239b3082
fredz999 <jjjj58@hotmail.com>
2023-05-15 08:23:42 +0100

    Initial Commit

---
+++ nGon_Generator/nGon_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  nGon_GeneratorApp.swift
+//  nGon_Generator
+//
+//  Created by Jon on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct nGon_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
